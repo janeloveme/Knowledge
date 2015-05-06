fc0caebfde1c4585f68fc9b41e6fc3f777e7f329
janeloveme <438427112@qq.com>
2015-05-06 18:07:53 +0800

    增加 Swift 训练项目
    1. add PlayGround /Contents.swift
    2. add other Xcode project

---
+++ Swift/training.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, swift"
+println(str)
+var i = 0;
+var j = 1;
+for(i = 0;i<3; i++)
+{
+    j++
+    println("CHRIS LOVE ME")
+    println(j)
+}
